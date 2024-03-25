a88f808a1146e10720cf38fd568578e5d533f57d
Айтурган Кадыралиева <ajturgankadyralieva@Ajturgans-MacBook-Pro.local>
2024-03-25 20:58:55 +0600

    task4 done

--- main.py
+++ main.py
@@ -21,16 +21,44 @@
 # print(mylist)
 
 # 2. 
-class Star:
-    def __init__(self, text) -> None:
-        self.text = text
+# class Star:
+#     def __init__(self, text) -> None:
+#         self.text = text
     
-    def replace_space_to_star(self):
-        result = ''
-        for t in (self.text.strip().split()):
-            result += t
-            result += '*'
-        print(result[:-1])
+#     def replace_space_to_star(self):
+#         result = ''
+#         for t in (self.text.strip().split()):
+#             result += t
+#             result += '*'
+#         print(result[:-1])
 
-star = Star('     Hello World!  ')
-star.replace_space_to_star()
+# star = Star('     Hello World!  ')
+# star.replace_space_to_star()
+
+# 3.
+# import os
+
+# class Path:
+#     def __init__(self, path) -> None:
+#         self.path = path
+
+#     def files_ext(self):
+#             for name in os.listdir(path=self.path):
+#                 if os.path.isfile(os.path.join(self.path, name)):
+#                     if name.split('.')[-1] in ('pdf','txt','py','epub'):
+#                         print(f'{name} - расширение {name.split('.')[-1]}')
+
+# ath = Path('/Users/ajturgankadyralieva/Desktop')
+# ath.files_ext()
+
+# 4.
+class Number:
+    def __init__(self, number) -> None:
+        self.number = number
+    def get_num_sum(self):
+        result = 0
+        for n in str(self.num):
+            result += int(n)
+        return result
+number = Number(number=35732)
+print(number.get_num_sum())
